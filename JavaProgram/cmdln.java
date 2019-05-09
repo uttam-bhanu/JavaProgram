4306c37afec4faff2c007899855e1339b6d2cca2
uttam-bhanu <uttam4ever@gmail>
2019-05-09 16:54:22 +0530

    hh

---
+++ JavaProgram/cmdln.java
@@ -0,0 +1,15 @@
+
+public class cmdln {
+
+	public static void main(String[] args) {
+		int s = 0;
+		for (int i =0; i <args.length; i++)
+		{
+			s = s + Integer.parseInt(args[i]);
+			
+					
+		}
+		System.out.println(s);
+	}
+
+}
